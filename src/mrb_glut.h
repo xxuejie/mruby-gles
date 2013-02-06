d523fa3e78256153cf5b723835507a5f209f6dd9
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2013-02-05 22:57:21 -0500

    Okay, I finally decided to remove GLUT. Currently I do not want to waste time on this. We will just rely on the platform-specific code to setup render context. However, we may come back to this if at some later time we found this quite useful.

--- src/mrb_glut.h
+++
@@ -1,6 +0,0 @@
-#ifndef _mrb_glut_h_
-#define _mrb_glut_h_
-
-void mrb_mruby_gles_gem_glut_init(mrb_state* mrb);
-
-#endif
--- src/mrb_init.c
+++ src/mrb_init.c
@@ -2,16 +2,11 @@
 
 #include "mrb_gl2.h"
 #include "mrb_gl2ext.h"
-#include "mrb_glut.h"
 
 void
 mrb_mruby_gles_gem_init(mrb_state* mrb)
 {
   mrb_mruby_gles_gem_gl2_init(mrb);
-
-#ifdef MRBGEM_GLES_ENABLE_GLUT
-  mrb_mruby_gles_gem_glut_init(mrb);
-#endif
 
 #ifdef MRBGEM_GLES_ENABLE_EXT
   mrb_mruby_gles_gem_gl2ext_init(mrb);
